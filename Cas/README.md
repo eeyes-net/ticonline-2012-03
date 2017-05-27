23bd74f6748163f84b415256d66a518291a23e22
ganlvtech <ganlvtech@qq.com>
2017-05-27 21:24:54 +0800

    CAS模块

---
+++ Cas/README.md
@@ -0,0 +1,10 @@
+# CAS登录模块
+
+## 目录结构说明
+
+* `CAS.php`和`CAS`目录：Jasig官方提供的phpCAS库
+* `config.php`：西安交通大学的CAS配置
+
+## 其他
+
+此README有Ganlv与2017-03-23创建，原来的代码中不包括此文件
---
+++ Cas/config.php
@@ -0,0 +1,19 @@
+<?php
+
+class casConfig
+{
+    public function get_host()
+    {
+        return $cas_host = 'cas.xjtu.edu.cn';
+    }
+
+    public function get_context()
+    {
+        return $cas_context = '';
+    }
+
+    public function get_port()
+    {
+        return $cas_port = 443;
+    }
+}
